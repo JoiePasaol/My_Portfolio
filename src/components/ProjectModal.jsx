30a8c2edf61897dca561901ada4e1f8b73c7b24a
JoiePasaol <jandelbjorn74@gmail.com>
2025-08-07 17:21:47 -0700

    Update ProjectModal.jsx

--- src/components/ProjectModal.jsx
+++ src/components/ProjectModal.jsx
@@ -56,25 +56,35 @@ const ProjectModal = ({ project, onClose }) => {
               <img
                 src={project.images[currentImageIndex]}
                 alt={`${project.title} - Image ${currentImageIndex + 1}`}
-                className="w-full h-full object-cover transition-transform duration-300"
+                className="w-full h-full object-contain transition-transform duration-300"
               />
 
               {/* Navigation Arrows */}
               {project.images.length > 1 && (
-                <>
+                <div
+                  className="absolute inset-0"
+                  onKeyDown={(e) => {
+                    if (e.key === "ArrowLeft") {
+                      prevImage();
+                    } else if (e.key === "ArrowRight") {
+                      nextImage();
+                    }
+                  }}
+                  tabIndex={-1}
+                >
                   <button
-                    className="absolute left-4 top-1/2 -translate-y-1/2 bg-white hover:bg-white text-black shadow-lg p-2 rounded-full cursor-pointer"
+                    className="absolute left-4 top-1/2 -translate-y-1/2 bg-white hover:bg-white text-black shadow-lg p-2 rounded-full cursor-pointer "
                     onClick={prevImage}
                   >
                     <ChevronLeft className="h-6 w-6" />
                   </button>
                   <button
-                    className="absolute right-4 top-1/2 -translate-y-1/2 bg-white hover:bg-white text-black shadow-lg p-2 rounded-full cursor-pointer"
+                    className="absolute right-4 top-1/2 -translate-y-1/2 bg-white hover:bg-white text-black shadow-lg p-2 rounded-full cursor-pointer "
                     onClick={nextImage}
                   >
                     <ChevronRight className="h-6 w-6" />
                   </button>
-                </>
+                </div>
               )}
 
               {/* Image Indicators */}
