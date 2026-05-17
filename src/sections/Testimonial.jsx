aad982e990ecd0a8e59285dcaa71c2ac11e2efef
Joie Pasaol <joie.pasaol13@gmail.com>
2026-05-17 22:22:41 +0800

    done

--- src/sections/Testimonial.jsx
+++ src/sections/Testimonial.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect, useCallback, memo, useMemo } from "react";
 import { createClient } from "@supabase/supabase-js";
 import Swal from "sweetalert2";
 
-// Inisialisasi Supabase client
+
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
